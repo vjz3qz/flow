64ef5447b2f1fc0cfde84bf34af904769b04e077
vjz3qz <vjz3qz@virginia.edu>
2024-02-02 23:36:57 -0500

    server side auth

--- backend/config/supabase.js
+++ backend/config/supabase.js
@@ -2,7 +2,14 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY
+  process.env.SUPABASE_KEY,
+  {
+    auth: {
+      autoRefreshToken: false,
+      persistSession: false,
+      detectSessionInUrl: false,
+    },
+  }
 );
 
 export { supabase };
